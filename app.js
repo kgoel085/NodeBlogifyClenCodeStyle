941e64e29daef9e7f46ce01235877791b2ebc687
kgoel085 <kgoel085@gmail.com>
2019-12-29 19:58:31 +0530

    Added module loaders

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const loaders = require('./loaders')
+const config = require('./config')
+
+const startServer = async () => {
+  // Main app
+  const app = express()
+
+  // Load modules one by one
+  await loaders(app)
+
+  app.listen(config.PORT, err => {
+    if (err) return console.log('Error starting app')
+
+    console.log(`Your app is ready to server on port ${config.PORT}`)
+  })
+}
+
+// Start the server
+startServer()
