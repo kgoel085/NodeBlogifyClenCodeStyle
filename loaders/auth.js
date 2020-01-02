540e871918ce2e584a5de6baba58566b277a8b15
kgoel085 <kgoel085@gmail.com>
2020-01-03 01:42:26 +0530

    Fixed user not being matched with string value

--- loaders/auth.js
+++ loaders/auth.js
@@ -3,19 +3,26 @@ const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 const { secret } = require('./../config')
 const UserModel = require('./../modules/User')
 
+const errorMsg = 'Unauthorized access'
 module.exports = async app => {
   // JWT Passport Strategy
   passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: secret
-  }, async (payload, done) => {
+    secretOrKey: secret,
+    passReqToCallback: true
+  }, async (request, payload, done) => {
+    // const { req, res } = request
     try {
-      const user = await UserModel.findOne({ _id: payload.sub })
-      if (!user) return done(null, false) // If user is not present
+      // Database Object
+      const { makeId } = await UserModel.db
+
+      const UserId = makeId(payload.sub)
+      const user = await UserModel.findOne({ _id: UserId })
+      if (!user) done(null, false, errorMsg)
 
       done(null, user)
     } catch (err) {
-      done(err, false)
+      done(err, false, err.message)
     }
   }))
 
