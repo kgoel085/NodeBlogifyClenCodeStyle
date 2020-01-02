727a98d4e656695df28f8b1d61e3da849f707fec
= <=>
2020-01-02 17:12:22 +0530

    Added JWT dependency

--- modules/User/index.js
+++ modules/User/index.js
@@ -2,5 +2,6 @@ const Schema = require('./schema')
 const Model = require('./use-case')
 const Db = require('./../../loaders/database')
 const Encrypt = require('./../../services/Encryption')
+const JWT = require('./../../services/JWT')
 
-module.exports = new Model(Db(), Schema, Encrypt)
+module.exports = new Model(Db(), Schema, Encrypt, JWT)
