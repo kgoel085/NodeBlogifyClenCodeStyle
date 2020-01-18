237c8b2d256f93481f775ceec813b6cda857b2ea
= <=>
2020-01-18 15:50:19 +0530

    Updated category schema

--- modules/User/schema.js
+++ modules/User/schema.js
@@ -1,61 +1,33 @@
-const { isRequired, validateEmail, validateString } = require('./../../helpers')
+const validateSchema = require('./../../helpers/schema')
 
-const validateObj = ({
-  username = isRequired('username'),
-  email = isRequired('email'),
-  createdAt = new Date().getTime(),
-  modifiedAt = null,
-  isActive = true,
-  tokens = [],
-  ...OtherInfo
-} = {}) => {
-  username = validateString('username', username)
-  email = validateEmail('email', email)
-
-  return {
-    username,
-    email,
-    createdAt,
-    modifiedAt,
-    isActive,
-    tokens,
-    ...OtherInfo
-  }
-}
-
-const normalizeObj = ({
-  username,
-  email,
-  createdAt,
-  modifiedAt,
-  isActive,
-  confirmPassword,
-  password,
-  tokens,
-  ...otherInfo
-} = {}) => {
-  // username = normalizeString(username)
-  if (createdAt) createdAt = createdAt.toString()
-  if (modifiedAt) modifiedAt = modifiedAt.toString()
-
-  return {
-    username,
-    email,
-    createdAt,
-    modifiedAt,
-    isActive,
-    password,
-    tokens,
-    ...otherInfo
-  }
+const schema = {
+  username: {
+    required: true,
+    type: 'string'
+  },
+  email: {
+    required: true,
+    type: 'email'
+  },
+  createdAt: {
+    type: 'date',
+    default: new Date().getTime()
+  },
+  modifiedAt: {
+    type: 'date',
+    default: null
+  },
+  isActive: {
+    type: 'boolean',
+    default: true
+  },
+  password: undefined,
+  confirmPassword: undefined,
+  tokens: undefined
 }
 
 module.exports = (userObj) => {
   // Validate Object
-  const validObj = validateObj(userObj)
-
-  // Normalize Object
-  const normalizedObj = normalizeObj(validObj)
-
-  return normalizedObj
+  const validObj = validateSchema(schema, userObj)
+  return validObj
 }
