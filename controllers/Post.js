01732789c5a5a20f670cb4de5f1692be9b92d41a
kgoel085 <kgoel085@gmail.com>
2020-02-07 01:07:25 +0530

    Added Posts add method

--- controllers/Post.js
+++ controllers/Post.js
@@ -1,5 +1,9 @@
 const Post = require('./../modules/Post')
 module.exports = {
   // Create post
-  createPost: async (req, res, nxt) => {}
+  createPost: async (req, res, nxt) => {
+    return Post.createPost(req.body)
+      .then(resp => res.status(resp.statusCode).json(resp))
+      .catch(err => nxt(err))
+  }
 }
--- modules/Post/use-case.js
+++ modules/Post/use-case.js
@@ -1,4 +1,6 @@
 const CRUD = require('./../../services/CRUD')
+const { UniqueConstraint, InvalidParam } = require('./../../helpers/error')
+const { isType } = require('./../../helpers')
 
 class Post extends CRUD {
   constructor (db = null, schema = null, category = null) {
@@ -8,8 +10,35 @@ class Post extends CRUD {
 
   // Create category
   async createPost (obj = {}) {
-    obj = this.schema(obj)
-    return obj
+    obj = this.schema(obj, true)
+
+    // Validate values
+    const { name, category } = obj
+
+    // Validate & check for post name existence
+    const { data: nameExists } = await this.checkForValue(name, 'name', false, true)
+    if (nameExists) throw new UniqueConstraint(`Post with name (${name}) already exists !`)
+
+    // Validate & check for categories
+    await this.validatePostCategories(category)
+
+    const result = await this.insert(obj)
+    return result
+  }
+
+  // Validate & check provided categories are valid or not
+  async validatePostCategories (catArr = []) {
+    isType('array', catArr, 'category', true)
+
+    const returnVal = []
+    for (const category of catArr) {
+      const { data } = await this.category.checkCategory(category, true)
+      if (!data) throw new InvalidParam('Invalid category provided !')
+
+      returnVal.push(data)
+    }
+
+    return returnVal
   }
 }
 
