ee24ed1b6a85fdba407b31d49ab31dc7ce87d9cd
kgoel085 <kgoel085@gmail.com>
2020-02-07 00:42:30 +0530

    Added helmet for HTTP secure headers

--- loaders/express.js
+++ loaders/express.js
@@ -1,7 +1,11 @@
 const bodyParser = require('body-parser')
 const routes = require('./../routes')
+const helmet = require('helmet')
 
 module.exports = app => {
+  // Helmet secure http headers
+  app.use(helmet())
+
   // Body parser
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json())
