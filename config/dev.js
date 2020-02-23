3e8d4ac6bd4605ec5540ab37febd35eb738afd0e
kgoel085 <kgoel085@gmail.com>
2020-02-24 01:42:21 +0530

    Changed config values

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: 3000,
-  secret: '123456789',
+  secret: '5n3GPA5WUHXgcm4N2FnP6f3UY5aQexLP',
   expiryTime: '60m', // In Minutes or Formatted string -- 1h, 15m
   globalNamespace: 'reqGlobals',
   DB: {
