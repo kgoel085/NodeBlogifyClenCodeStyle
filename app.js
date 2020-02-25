2bc9e8cf37f06e5ba3c6164bec2b67d3ca97e63e
kgoel085 <kgoel085@gmail.com>
2020-02-26 01:12:27 +0530

    Chanegd default ipv6 to ipv4

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const startServer = async () => {
   // Load modules one by one
   await loaders(app)
 
-  app.listen(config.PORT, err => {
+  app.listen(config.PORT, '0.0.0.0', err => {
     if (err) return console.log('Error starting app')
 
     console.log(`Your app is ready to server on port ${config.PORT}`)
